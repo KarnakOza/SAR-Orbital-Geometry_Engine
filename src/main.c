3b0d431961659d75e4078244b94ac23cc76fe635
Fermy <123935594+KarnakOza@users.noreply.github.com>
2026-03-19 23:44:11 +0530

    Create main.c

---
+++ src/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+#include "constants.h"
+#include "sar_range.h"
+#include "vec3.h"
+
+
+double orbital_velocity(double altitude);
+double orbital_period(double altitude);
+double doppler_centroid(double velocity, double wavelength, double angle);
+
+double slant_range(double xs, double ys, double zs,
+                   double xg, double yg, double zg);
+
+/*void satellite_position_eci(double a, double e, double i,
+                            double raan, double omega, double nu,
+                            double *x, double *y, double *z);  */
